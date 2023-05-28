b261884b918849efdbf9ed4196532701593b0525
Frank Hunleth <fhunleth@troodon-software.com>
2023-05-28 18:27:21 -0400

    Defer loading the NIF until opening a GPIO
    This change moves the NIF load from the time at which the module is
    loaded to the time when a GPIO actually needs to be used.
    
    The primary motivation for doing this is to defer native code crashes
    from happening at load time to the time of first use. Load time is
    harder to debug and sometimes its not clear which NIF caused the crash.
    
    The calls to `apply` get around some complexity with ignoring Dialyzer
    warnings. Dialyzer can't figure out that the recursive looking call
    actually invokes the NIF code.

--- lib/gpio/gpio_nif.ex
+++ lib/gpio/gpio_nif.ex
@@ -3,50 +3,32 @@
 # SPDX-License-Identifier: Apache-2.0
 
 defmodule Circuits.GPIO.Nif do
-  @on_load {:load_nif, 0}
-  @compile {:autoload, false}
-
   @moduledoc false
 
-  def load_nif() do
+  defp load_nif() do
     nif_binary = Application.app_dir(:circuits_gpio, "priv/gpio_nif")
-
     :erlang.load_nif(to_charlist(nif_binary), 0)
   end
 
-  def open(_pin_number, _pin_direction, _initial_value, _pull_mode) do
-    :erlang.nif_error(:nif_not_loaded)
+  def open(pin_number, pin_direction, initial_value, pull_mode) do
+    with :ok <- load_nif() do
+      apply(__MODULE__, :open, [pin_number, pin_direction, initial_value, pull_mode])
+    end
   end
 
-  def close(_gpio) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def close(_gpio), do: :erlang.nif_error(:nif_not_loaded)
+  def read(_gpio), do: :erlang.nif_error(:nif_not_loaded)
+  def write(_gpio, _value), do: :erlang.nif_error(:nif_not_loaded)
 
-  def read(_gpio) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def set_interrupts(_gpio, _trigger, _suppress_glitches, _process),
+    do: :erlang.nif_error(:nif_not_loaded)
 
-  def write(_gpio, _value) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def set_interrupts(_gpio, _trigger, _suppress_glitches, _process) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def set_direction(_gpio, _pin_direction) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def set_pull_mode(_gpio, _pull_mode) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def pin(_gpio) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def set_direction(_gpio, _pin_direction), do: :erlang.nif_error(:nif_not_loaded)
+  def set_pull_mode(_gpio, _pull_mode), do: :erlang.nif_error(:nif_not_loaded)
+  def pin(_gpio), do: :erlang.nif_error(:nif_not_loaded)
 
   def info() do
-    :erlang.nif_error(:nif_not_loaded)
+    :ok = load_nif()
+    apply(__MODULE__, :info, [])
   end
 end
