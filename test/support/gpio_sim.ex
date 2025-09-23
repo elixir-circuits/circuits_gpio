92cdabbe88573e5e44b13993966226e083e72198
Frank Hunleth <fhunleth@troodon-software.com>
2025-09-23 08:27:03 -0400

    Minor code improvements to gpio_sim

--- test/support/gpio_sim.ex
+++ test/support/gpio_sim.ex
@@ -1,21 +1,26 @@
 defmodule GPIOSim do
   alias Circuits.GPIO
 
-  defp line_path(gpio_name) do
+  @spec line_path(String.t()) :: String.t()
+  def line_path(gpio_name) do
     {:ok, %{location: {gpiochip, line}, controller: controller}} = GPIO.identifiers(gpio_name)
     base_controller = String.replace(controller, "-node0", "")
     "/sys/devices/platform/#{base_controller}/#{gpiochip}/sim_gpio#{line}"
   end
 
+  @spec write(String.t(), 0 | 1) :: :ok
   def write(gpio_name, value) do
     pull_path = Path.join(line_path(gpio_name), "pull")
     pull_dir = if value > 0, do: "pull-up", else: "pull-down"
     File.write!(pull_path, pull_dir)
   end
 
+  @spec read(String.t()) :: 0 | 1
   def read(gpio_name) do
     value_path = Path.join(line_path(gpio_name), "value")
-    {value_str, 0} = System.cmd("cat", [value_path])
-    String.trim(value_str) |> String.to_integer()
+    File.read!(value_path) |> value_to_int()
   end
+
+  defp value_to_int("0" <> _), do: 0
+  defp value_to_int("1" <> _), do: 1
 end
